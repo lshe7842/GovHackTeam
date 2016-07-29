2b74e1b88cac73e72aceb5f56d26e902aaaa8e56
william.xu <william.xu@iag.com.au>
2016-07-30 08:44:46 +1000

    switching for node-sass so we don't need ruby

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	server = require('gulp-express'),
 	source = require('vinyl-source-stream'),
     sass = require('gulp-ruby-sass'),
+		nodesass = require('gulp-sass'),
     rename = require('gulp-rename'),
 	watchify = require('watchify'),
 	browserify = require('browserify');
@@ -18,14 +19,16 @@ gulp.task('bundle-client', function(){
         var stream = b.bundle();
         return stream
             .pipe(source('bundle.js'))
-            .pipe(gulp.dest('public/js/')); 
+            .pipe(gulp.dest('public/js/'));
     };
 
     return rebundle();
 });
 
 gulp.task('css', function(){
-    return sass('./sass/main/main.scss', { style: 'compressed' })
+    //return sass('./sass/main/main.scss', { style: 'compressed' })
+		return gulp.src('./sass/main/main.scss')
+				.pipe(nodesass().on('error', nodesass.logError))
         .pipe(rename('styles.css'))
         .pipe(gulp.dest('./public/css'));
 });
@@ -34,9 +37,9 @@ gulp.task('server', function(){
 	server.run(['app.js']);
 
     gulp.watch('./sass/**/*.scss', ['css']);
-    gulp.watch(['./public/js/*.js'], server.notify);
-    gulp.watch(['./public/css/*.css'], server.notify);
-    gulp.watch(['./templates/*.hbs'], server.notify);
+    gulp.watch(['./public/js/*.js'], { usePolling: true }, server.notify);
+    gulp.watch(['./public/css/*.css'], { usePolling: true }, server.notify);
+    gulp.watch(['./templates/*.hbs'], { usePolling: true }, server.notify);
 });
 
 gulp.task('default', ['bundle-client', 'css', 'server']);
